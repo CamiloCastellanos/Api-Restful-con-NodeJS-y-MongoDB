86b16286574445e89ae46cea497bcfac3929aa76
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-06-30 14:18:56 -0500

    Documentacion de clase

--- models/fruta.js
+++ models/fruta.js
@@ -10,5 +10,10 @@ let frutaSchema = schema({
     temporada: Boolean
 });
 
-
+/*
+Recordatorio:
+- Si no existe la BD la genera.
+- La BD se genera toda en minusculas y en plural
+(Esto lo hace automaticamente la libreria de mondoose)
+*/
 module.exports = mongoose.model('Fruta', frutaSchema);
