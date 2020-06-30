d6ec61f5e345a5b1bced6e89ce94f63d029150ea
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-06-30 14:00:56 -0500

    Adiciona nueva ruta "Adicionar Fruta"

--- routes/frutas.js
+++ routes/frutas.js
@@ -6,6 +6,6 @@ let frutaController = require('../controllers/fruta');
 let api = express.Router();
 
 api.get('/pruebas', frutaController.prueba);
-
+api.post('/fruta', frutaController.adicionarFruta);
 
 module.exports = api;
