1272e27580383b6b3a2e822f4426f6d4524ca500
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-06-30 14:00:31 -0500

    Creadion de clase controller para BD Fruta

---
+++ controllers/fruta.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const { request } = require("../app");
+
+//Importamos el Modelo de la BD de Mongo
+let frutaModelo = require('../models/fruta');
+
+/*Funciones */
+function prueba(req, res) {
+    res.status(200).send({
+        message: 'Esta es la ruta por defencto del api REST con MongoDB y NodeJS.'
+    });
+}
+//Adiciona una fruta en la BD.
+function adicionarFruta(req, res) {
+    let fruta = new frutaModelo();
+    let params = req.body;
+
+    if (params.nombre) {
+        fruta.nombre = params.nombre;
+        fruta.color = params.color;
+        fruta.temporada = params.temporada;
+
+        fruta.save((err, frutaStored) => {
+            if (err) {
+                res.status(500).send({
+                    message: 'Error en el servicio.'
+                });
+            } else {
+                if (frutaStored) {
+                    res.status(200).send({
+                        fruta: frutaStored
+                    });
+                } else {
+                    res.status(200).send({
+                        message: 'No se ha Guardado la fruta.'
+                    });
+                }
+            }
+        });
+    } else {
+        res.status(200).send({
+            message: 'El nombre de la fruta es obligatorio.'
+        });
+    }
+}
+
+/* Exportar las funciones*/
+module.exports = {
+    prueba,
+    adicionarFruta
+}
